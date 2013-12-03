8e069cd0fccd0c7ef8cd658ff58a666289f243bc
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-03 10:06:34 +0100

    Added QuestionView as tableviewcontroller

--- QuizzingBricks/QuizzingBricks/QBQuestionViewController.h
+++ QuizzingBricks/QuizzingBricks/QBQuestionViewController.h
@@ -2,12 +2,12 @@
 //  QBQuestionViewController.h
 //  QuizzingBricks
 //
-//  Created by Linus Hedenberg on 2013-10-23.
+//  Created by Linus Hedenberg on 2013-12-03.
 //  Copyright (c) 2013 Linus Hedenberg. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface QBQuestionViewController : UIViewController
+@interface QBQuestionViewController : UITableViewController
 
 @end
