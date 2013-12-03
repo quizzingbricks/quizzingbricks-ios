e36d631b0dbd94a38d496637e59913dc0d683927
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-03 11:04:00 +0100

    Removed question-id.

--- QuizzingBricks/QuizzingBricks/QBQuestion.m
+++ QuizzingBricks/QuizzingBricks/QBQuestion.m
@@ -10,11 +10,10 @@
 
 @implementation QBQuestion
 
-- (id)initWithQID:(NSString *)qID question:(NSString *)question answer1:(NSString *)a1 answer2:(NSString *)a2 answer3:(NSString *)a3 answer4:(NSString *)a4
+- (id)initWithQuestion:(NSString *)question answer1:(NSString *)a1 answer2:(NSString *)a2 answer3:(NSString *)a3 answer4:(NSString *)a4
 {
     self = [super init];
     if (self) {
-        self.qID = qID;
         self.question = question;
         self.answer1 = a1;
         self.answer2 = a2;
