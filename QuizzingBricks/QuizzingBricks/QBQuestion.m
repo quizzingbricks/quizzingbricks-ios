9080d09159a69f858e3da87c0939ad3d5f54e65a
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-03 10:39:48 +0100

    Added Question class

---
+++ QuizzingBricks/QuizzingBricks/QBQuestion.m
@@ -0,0 +1,13 @@
+//
+//  QBQuestion.m
+//  QuizzingBricks
+//
+//  Created by Linus Hedenberg on 2013-12-03.
+//  Copyright (c) 2013 Linus Hedenberg. All rights reserved.
+//
+
+#import "QBQuestion.h"
+
+@implementation QBQuestion
+
+@end
