4b278a619dbad673adc32d1cc08e0d13ea3e6c78
Linus Hedenberg <linushedenberg@gmail.com>
2013-12-03 10:44:09 +0100

    Added Question class

--- QuizzingBricks/QuizzingBricks/QBQuestion.h
+++ QuizzingBricks/QuizzingBricks/QBQuestion.h
@@ -10,4 +10,13 @@
 
 @interface QBQuestion : NSObject
 
+@property (strong, nonatomic) NSString *qID;
+@property (strong, nonatomic) NSString *question;
+@property (strong, nonatomic) NSString *answer1;
+@property (strong, nonatomic) NSString *answer2;
+@property (strong, nonatomic) NSString *answer3;
+@property (strong, nonatomic) NSString *answer4;
+
+- (id)initWithQID:(NSString *)qID question:(NSString *)question answer1:(NSString *)a1 answer2:(NSString *)a2 answer3:(NSString *)a3 answer4:(NSString *)a4;
+
 @end
--- QuizzingBricks/QuizzingBricks/QBQuestion.m
+++ QuizzingBricks/QuizzingBricks/QBQuestion.m
@@ -10,4 +10,18 @@
 
 @implementation QBQuestion
 
+- (id)initWithQID:(NSString *)qID question:(NSString *)question answer1:(NSString *)a1 answer2:(NSString *)a2 answer3:(NSString *)a3 answer4:(NSString *)a4
+{
+    self = [super init];
+    if (self) {
+        self.qID = qID;
+        self.question = question;
+        self.answer1 = a1;
+        self.answer2 = a2;
+        self.answer3 = a3;
+        self.answer4 = a4;
+    }
+    return self;
+}
+
 @end
